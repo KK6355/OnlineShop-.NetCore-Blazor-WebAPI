2a965c6a6f633a1d1b1ee47c15d915f9d598993a
vicky kang <vicky.kang@payitnow.info>
2023-12-28 13:58:16 +1300

    Added pagination on product list page

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -13,6 +13,9 @@ namespace BlazorEcommerce.Client.Services.ProductService
 		}
         public List<Product> Products { get; set; } = new List<Product>();
 		public string Message { get; set; } = "Loading products...";
+		public int CurrentPage { get ; set; } = 1;
+		public int PageCount { get; set; } = 0;
+		public string LastSearchText { get; set ; } =string.Empty;
 
 		public event Action ProductsChanged;
 
@@ -28,6 +31,10 @@ namespace BlazorEcommerce.Client.Services.ProductService
 				await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product/featured") :
 				await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
 			if(result != null && result.Data != null) Products = result.Data;
+			CurrentPage = 1;
+			PageCount = 0;
+			if(Products.Count==0)
+				Message = "No products found";
 			ProductsChanged.Invoke();
 		}
 
@@ -37,13 +44,21 @@ namespace BlazorEcommerce.Client.Services.ProductService
 			return result.Data;
 		}
 
-		public async Task SearchProducts(string searchText)
+		public async Task SearchProducts(string searchText,int page=1)
 		{
-			var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
-			if (result != null && result.Data != null) Products = result.Data;
+			LastSearchText = searchText;
+			var result = await _http.GetFromJsonAsync<ServiceResponse<ProductsSearchResult>>($"api/product/search/{searchText}/{page}");
+			if (result != null && result.Data.Products != null)
+			{
+				Products = result.Data.Products;
+				CurrentPage = result.Data.CurrentPage;
+				PageCount = result.Data.Pages;
+			}
 			if(Products.Count==0) Message = "No product found";
 			ProductsChanged?.Invoke();
 			
 		}
+
+		
 	}
 }
--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -78,11 +78,20 @@ namespace BlazorEcommerce.Server.Services.ProductService
 
 		}
 
-		public async Task<ServiceResponse<List<Product>>> SearchProducts(string searchText)
+		public async Task<ServiceResponse<ProductsSearchResult>> SearchProducts(string searchText, int page)
 		{
-			var response = new ServiceResponse<List<Product>>
+			var pageResults = 2f;
+			var pageCount = Math.Ceiling((await FindProductsBySearchText(searchText)).Count / pageResults);
+			var products = await _context.Products.Where(p => p.Title.ToLower().Contains(searchText.ToLower())
+							|| p.Description.ToLower().Contains(searchText.ToLower())).Include(p => p.Variants).Skip((page-1)*(int)pageResults).Take((int)pageResults).ToListAsync();
+
+			var response = new ServiceResponse<ProductsSearchResult>
 			{
-				Data = await FindProductsBySearchText(searchText)
+				Data = new ProductsSearchResult { 
+					Products = products,
+					CurrentPage = page,
+					Pages = (int)pageResults,
+				}
 
 			};
 			return response;
